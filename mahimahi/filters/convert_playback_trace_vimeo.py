8db1361b848631d46439855263d74c22d45fd52d
vk <vayu@stanford.edu>
2017-05-19 00:45:36 -0700

    playback rate for vimeo

--- mahimahi/filters/convert_playback_trace_vimeo.py
+++ mahimahi/filters/convert_playback_trace_vimeo.py
@@ -10,10 +10,26 @@ start_str = time_file.read()
 firstdt = (dateutil.parser.parse(start_str))
 time_file.close()
 
+
+ratemap = {}
+r = open('data/vimeo_rates')
+for line in r:
+    tokens = line.split()
+    identifier = tokens[0].split('-')[-1]
+    rate = (tokens[5].rstrip('k,'))
+    print tokens
+    ratemap[identifier] = rate
+r.close()
+
+print ratemap
+
+
 f = open(sys.argv[1])
 js = json.load(f)
 for j in js:
     if j: 
         dt = dateutil.parser.parse(j["time"])
-        print (dt-firstdt).total_seconds(), float(j["responseSize"])*8/1000/SECS_PER_SEGMENT # convert bytes to kbps
+        kbps =  ratemap[j["request"].split("/")[-3]]
+        deduced_kbps = float(j["responseSize"])*8/1000/SECS_PER_SEGMENT # convert bytes to kbps
+        print (dt-firstdt).total_seconds(), kbps
 f.close()
---
+++ mahimahi/generate_auxillary_data_vimeo.sh
@@ -0,0 +1,1 @@
+youtube-dl -F "${1}" | grep -i "DASH video"  > data/vimeo_rates
