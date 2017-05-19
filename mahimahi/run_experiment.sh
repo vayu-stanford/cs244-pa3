404b7d81649fe4b53058c29949546cdd844f2471
vk <vayu@stanford.edu>
2017-05-18 22:00:40 -0700

    cleaning up

--- mahimahi/run_experiment.sh
+++ mahimahi/run_experiment.sh
@@ -1,7 +1,7 @@
 URL='https://vimeo.com/217874316'
 #URL='https://www.youtube.com/watch?v=RDfjXj5EGqI'
 echo Starting browser connection to ${URL}
-mm-meter --meter-downlink firefox  ${URL} &> /dev/null &
-sleep 20
+mm-meter --meter-downlink -- firefox  --devtools ${URL} &> /dev/null &
+sleep 60
 echo Starting long lived tcp connection to download video at ${URL}
-mm-meter --meter-downlink youtube-dl ${URL} &> /dev/null &
+mm-meter --meter-downlink -- youtube-dl ${URL} -o /dev/null &> /dev/null &
