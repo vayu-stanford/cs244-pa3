fbcae4c68fd5ddd9ab358543625633175287dce4
vk <vayu@stanford.edu>
2017-05-18 21:08:26 -0700

    revise sleep time

--- mahimahi/run_experiment.sh
+++ mahimahi/run_experiment.sh
@@ -2,6 +2,6 @@ URL='https://vimeo.com/217874316'
 #URL='https://www.youtube.com/watch?v=RDfjXj5EGqI'
 echo Starting browser connection to ${URL}
 mm-meter --meter-downlink firefox  ${URL} &> /dev/null &
-sleep 60
+sleep 20
 echo Starting long lived tcp connection to download video at ${URL}
 mm-meter --meter-downlink youtube-dl ${URL} &> /dev/null &
