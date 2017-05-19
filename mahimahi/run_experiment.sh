8607a6d5169fdd9323020e7e0530fe053af63b25
vk <vayu@stanford.edu>
2017-05-19 00:50:15 -0700

    finalizing vimeo

--- mahimahi/run_experiment.sh
+++ mahimahi/run_experiment.sh
@@ -2,7 +2,7 @@ URL='https://vimeo.com/217874316'
 SOURCE=vimeo
 WAIT_TIME=40
 #URL='https://www.youtube.com/watch?v=RDfjXj5EGqI'
-./generate_auxillary_data_${SOURCE}.sh
+./generate_auxillary_data_${SOURCE}.sh ${URL}
 ./current_time.py > data/${SOURCE}_start_time
 echo Starting browser connection to ${URL}
 mm-link ./traces/5mbps_trace ./traces/5mbps_trace --downlink-log="logs/${SOURCE}_client.raw.log" --meter-downlink -- firefox  --devtools ${URL}  &> /dev/null &
