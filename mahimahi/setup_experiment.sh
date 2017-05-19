82141a560a97a8215050b0b1f99b919b7b1d5482
vk <vayu@stanford.edu>
2017-05-18 22:15:07 -0700

    add logs fix 4 vs 5

--- mahimahi/setup_experiment.sh
+++ mahimahi/setup_experiment.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 mkdir -p data
-mm-link traces/4mbps_trace  traces/5mbps_trace --downlink-queue=droptail --downlink-queue-args="bytes=15000" --meter-downlink
+mkdir -p logs
+mm-link traces/5mbps_trace  traces/5mbps_trace --downlink-queue=droptail --downlink-queue-args="bytes=15000" --meter-downlink
