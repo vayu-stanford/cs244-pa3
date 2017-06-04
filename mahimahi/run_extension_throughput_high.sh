0043953d2dd967ed77649bc51f1a241a32c592c7
vk <vayu@stanford.edu>
2017-06-03 20:31:49 -0700

    buffer and throughput extensions

---
+++ mahimahi/run_extension_throughput_high.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+WAIT_TIME=60
+DL_KILL_TIME=80
+case $1 in 
+	"vimeo")
+		SOURCE=vimeo;; 
+	"youtube")
+		SOURCE=youtube;;
+	"local")
+		SOURCE=local;;
+	*) echo "Argument not recognized"
+		echo "Accepted arguments are:"
+		echo "vimeo"
+		echo "youtube"
+		echo "local"
+		exit -1;;
+esac
+sudo sysctl -w net.ipv4.ip_forward=1
+mkdir -p data
+mkdir -p logs
+mm-link traces/10mbps_trace  traces/10mbps_trace --meter-downlink -- ./scripts/run_experiment.sh "${SOURCE}" ${WAIT_TIME} ${DL_KILL_TIME}
